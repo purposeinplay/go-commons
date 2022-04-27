520ba75c477bec81fc8601f6c9d74b4e5412d54b
bradub <brad@win.com>
2022-04-27 17:21:47 +0300

    - added package comment for amount

--- amount/readme.go
+++ amount/readme.go
@@ -1,2 +1,15 @@
-// Package amount
+// Package amount implements an Amount type used to represent
+// a monetary amount defined by the following properties:
+//
+// - value, in the lowest denominator form, eg. cents for USD.
+//
+// - decimals, the number of the digits after
+// the decimals point, eg. 2 for USD.
+//
+// - currency code, the shorthand for
+// the currency, eg. USD for United States Dollar.
+//
+// It also implements a ValueSubunit type that is used to store,
+// persist and represent large monetary amounts in their
+// lowest denominator form.
 package amount
