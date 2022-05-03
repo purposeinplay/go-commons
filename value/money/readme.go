37a0cac0eb4f556475bc82c3a01242c7710e7158
bradub <brad@win.com>
2022-05-03 13:07:41 +0300

    - updated workflow actions versions

--- value/money/readme.go
+++ value/money/readme.go
@@ -1,7 +1,8 @@
 // Package money implements an Amount type used to represent
 // a monetary amount defined by the following properties:
 //
-// - value, in the lowest denominator form, eg. cents for USD.
+// - value, in the lowest denominator form,
+// eg. cents for USD.
 //
 // - decimals, the number of the digits after
 // the decimals point, eg. 2 for USD.
