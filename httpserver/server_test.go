02f951f5649b748bc6ea4a9c48e55974711209dc
bradub <brad@win.com>
2022-08-02 22:05:08 +0300

    fix(ci): fix ci tests and linting

--- httpserver/server_test.go
+++ httpserver/server_test.go
@@ -3,7 +3,6 @@ package httpserver_test
 import (
 	"context"
 	"fmt"
-	"go.uber.org/atomic"
 	"net"
 	"net/http"
 	"os"
@@ -15,6 +14,7 @@ import (
 	"github.com/purposeinplay/go-commons/httpserver"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
 	"go.uber.org/zap"
 )
 
