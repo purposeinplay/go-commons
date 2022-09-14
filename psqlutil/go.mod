a537330c96d96fafc860a04bb0dd3ac1450a2c13
bradub <adrian.brad@purposeinplay.com>
2022-09-14 20:52:04 +0300

    feat(psqlutil): add source files

---
+++ psqlutil/go.mod
@@ -0,0 +1,28 @@
+module github.com/purposeinplay/go-commons/psqlutil
+
+go 1.19
+
+require (
+	github.com/avast/retry-go v3.0.0+incompatible
+	github.com/jackc/pgx/v4 v4.16.1
+	go.uber.org/zap v1.23.0
+	gorm.io/driver/postgres v1.3.9
+	gorm.io/gorm v1.23.8
+	moul.io/zapgorm2 v1.1.3
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ psqlutil/psql_open.go
@@ -0,0 +1,120 @@
+package psqlutil
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/avast/retry-go"
+	// import for init function.
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"moul.io/zapgorm2"
+)
+
+// GormOpen opens a new db connection
+// and returns a *gorm.DB.
+func GormOpen(
+	ctx context.Context,
+	zapLogger *zap.Logger,
+	postgresDSN string,
+) (*gorm.DB, error) {
+	const (
+		connectionAttempts = 5
+		delaySeconds       = 2
+	)
+
+	var db *gorm.DB
+
+	err := retry.Do(func() error {
+		var err error
+
+		db, err = gorm.Open(
+			postgres.New(postgres.Config{
+				DriverName: "postgres",
+				DSN:        postgresDSN,
+			}),
+			&gorm.Config{
+				SkipDefaultTransaction: true,
+				Logger:                 zapgorm2.New(zapLogger),
+			})
+
+		return err
+	},
+		retry.Attempts(connectionAttempts),
+		retry.Delay(delaySeconds*time.Second),
+		retry.Context(ctx),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("open db: %w", err)
+	}
+
+	return db, nil
+}
+
+// SQLOpenWithGorm opens a new db connection and returns a
+// *sql.DB with a gorm driver.
+func SQLOpenWithGorm(
+	ctx context.Context,
+	postgresDSN string,
+) (*sql.DB, error) {
+	return sqlOpen(ctx, "postgres", postgresDSN)
+}
+
+// SQLOpenWithPgx opens a new db connection with a pgx driver.
+func SQLOpenWithPgx(
+	ctx context.Context,
+	postgresDSN string,
+) (*sql.DB, error) {
+	return sqlOpen(ctx, "pgx", postgresDSN)
+}
+
+// MustNewSQL panics if err is not nil, otherwise it returns db.
+func MustNewSQL(db *sql.DB, err error) *sql.DB {
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func sqlOpen(
+	ctx context.Context,
+	driver,
+	postgresDSN string,
+) (*sql.DB, error) {
+	const (
+		connectionAttempts = 5
+		delaySeconds       = 2
+	)
+
+	var db *sql.DB
+
+	err := retry.Do(func() error {
+		var err error
+
+		db, err = sql.Open(driver, postgresDSN)
+		if err != nil {
+			return fmt.Errorf("open: %w", err)
+		}
+
+		err = db.Ping()
+		if err != nil {
+			return fmt.Errorf("ping: %w", err)
+		}
+
+		return err
+	},
+		retry.Attempts(connectionAttempts),
+		retry.Delay(delaySeconds*time.Second),
+		retry.Context(ctx),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("open db: %w", err)
+	}
+
+	return db, nil
+}
