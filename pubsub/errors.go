21c365ad1264e2475a89859e609980d1237187d6
bradub <adrian.brad@purposeinplay.com>
2023-08-18 19:38:35 +0300

    fix(pubsub): remove unused error

--- pubsub/errors.go
+++ pubsub/errors.go
@@ -4,10 +4,5 @@ import (
 	"errors"
 )
 
-var (
-	// ErrExactlyOneChannelAllowed is returned a pubsub implementation supports only one channel.
-	ErrExactlyOneChannelAllowed = errors.New("exactly one channel allowed")
-	// ErrEventPayloadMustBeByteSlice is returned when an event payload is expected to be
-	// a byte slice and it's not.
-	ErrEventPayloadMustBeByteSlice = errors.New("event payload must be byte slice")
-)
+// ErrExactlyOneChannelAllowed is returned a pubsub implementation supports only one channel.
+var ErrExactlyOneChannelAllowed = errors.New("exactly one channel allowed")
