b6a657777574d02a208eb934a0870e0d8f06f793
bradub <adrian.brad@purposeinplay.com>
2023-10-16 17:23:56 +0300

    feat(pubsub): add pure sarama kafka publisher implementation

---
+++ pubsub/kafkasarama/kafkasarama_publisher.go
@@ -0,0 +1,76 @@
+package kafkasarama
+
+import (
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/purposeinplay/go-commons/pubsub"
+)
+
+var _ pubsub.Publisher[[]byte] = (*Publisher)(nil)
+
+// Publisher represents a kafka publisher.
+type Publisher struct {
+	logger       *slog.Logger
+	syncProducer sarama.SyncProducer
+}
+
+// NewPublisher creates a new kafka publisher.
+func NewPublisher(
+	logger *slog.Logger,
+	saramaConfig *sarama.Config,
+	brokers []string,
+) (*Publisher, error) {
+	cfg := saramaConfig
+
+	if cfg == nil {
+		cfg = sarama.NewConfig()
+
+		cfg.Producer.Retry.Max = 10
+		cfg.Producer.Return.Successes = true
+		cfg.Metadata.Retry.Backoff = time.Second * 2
+	}
+
+	producer, err := sarama.NewSyncProducer(brokers, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("new kafka publisher: %w", err)
+	}
+
+	return &Publisher{
+		logger:       logger,
+		syncProducer: producer,
+	}, nil
+}
+
+// Publish publishes an event to a kafka topic.
+func (p Publisher) Publish(event pubsub.Event[[]byte], channels ...string) error {
+	if len(channels) != 1 {
+		return pubsub.ErrExactlyOneChannelAllowed
+	}
+
+	topic := channels[0]
+
+	mes := &sarama.ProducerMessage{
+		Topic: topic,
+		Headers: []sarama.RecordHeader{
+			{
+				Key:   []byte("type"),
+				Value: []byte(event.Type),
+			},
+		},
+		Value: sarama.ByteEncoder(event.Payload),
+	}
+
+	if _, _, err := p.syncProducer.SendMessage(mes); err != nil {
+		return fmt.Errorf("publish: %w", err)
+	}
+
+	return nil
+}
+
+// Close closes the kafka publisher.
+func (p Publisher) Close() error {
+	return p.syncProducer.Close()
+}
