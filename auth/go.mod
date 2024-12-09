3aa9daca3b5b3bcb558501d3358d1dcb55729dff
bradub <adrian.brad@purposeinplay.com>
2024-12-09 11:02:41 +0200

    feat(auth): improvements and tests

--- auth/go.mod
+++ auth/go.mod
@@ -1,19 +1,24 @@
 module github.com/purposeinplay/go-commons/auth
 
-go 1.18
+go 1.22
+
+toolchain go1.23.4
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	go.uber.org/zap v1.26.0
-	google.golang.org/grpc v1.60.1
+	github.com/stretchr/testify v1.8.1
+	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.68.1
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240116215550-a9fa1716bcac // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241206012308-a4fef0638583 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- auth/middleware.go
+++ auth/middleware.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 )
 
+// Header keys.
 const (
-	HeaderUserID   = "X-USER-ID"
-	HeaderAppID    = "X-APP-ID"
-	HeaderUsername = "X-USERNAME"
+	HeaderUserID   = "X-User-Id"
+	HeaderAppID    = "X-App-Id"
+	HeaderUsername = "X-Username"
 )
 
 type contextKey int
@@ -21,6 +22,7 @@ const (
 	usernameCtxKey
 )
 
+// UserIDMiddlewareFunc is a middleware that extracts the user ID from the request.
 func UserIDMiddlewareFunc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		userID := r.Header.Get(HeaderUserID)
@@ -30,6 +32,7 @@ func UserIDMiddlewareFunc(next http.Handler) http.Handler {
 	})
 }
 
+// AppIDMiddlewareFunc is a middleware that extracts the app ID from the request.
 func AppIDMiddlewareFunc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		appID := r.Header.Get(HeaderAppID)
@@ -39,6 +42,7 @@ func AppIDMiddlewareFunc(next http.Handler) http.Handler {
 	})
 }
 
+// UsernameMiddlewareFunc is a middleware that extracts the username from the request.
 func UsernameMiddlewareFunc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		username := r.Header.Get(HeaderUsername)
@@ -48,6 +52,7 @@ func UsernameMiddlewareFunc(next http.Handler) http.Handler {
 	})
 }
 
+// NewUserIDContext returns a new context with the user ID.
 func NewUserIDContext(ctx context.Context, userID string) context.Context {
 	if userID == "" {
 		return ctx
@@ -56,6 +61,7 @@ func NewUserIDContext(ctx context.Context, userID string) context.Context {
 	return context.WithValue(ctx, userIDCtxKey, userID)
 }
 
+// NewAppIDContext returns a new context with the app ID.
 func NewAppIDContext(ctx context.Context, appID string) context.Context {
 	if appID == "" {
 		return ctx
@@ -64,6 +70,7 @@ func NewAppIDContext(ctx context.Context, appID string) context.Context {
 	return context.WithValue(ctx, appIDCtxKey, appID)
 }
 
+// NewUsernameContext returns a new context with the username.
 func NewUsernameContext(ctx context.Context, username string) context.Context {
 	if username == "" {
 		return ctx
@@ -72,19 +79,25 @@ func NewUsernameContext(ctx context.Context, username string) context.Context {
 	return context.WithValue(ctx, usernameCtxKey, usernameCtxKey)
 }
 
+// UserIDFromContext returns the user ID from the context.
 func UserIDFromContext(ctx context.Context) string {
+	// nolint: revive
 	userID, _ := ctx.Value(userIDCtxKey).(string)
 
 	return userID
 }
 
+// AppIDFromContext returns the app ID from the context.
 func AppIDFromContext(ctx context.Context) string {
+	// nolint: revive
 	appID, _ := ctx.Value(appIDCtxKey).(string)
 
 	return appID
 }
 
+// UsernameFromContext returns the username from the context.
 func UsernameFromContext(ctx context.Context) string {
+	// nolint: revive
 	username, _ := ctx.Value(usernameCtxKey).(string)
 
 	return username
