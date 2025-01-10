2df84eaa890841fae2ae29e054519e9abb6a7ac6
bradub <adrian.brad@purposeinplay.com>
2025-01-10 12:06:20 +0200

    feat(psqlutil): add errors gorm plugin

---
+++ psqlutil/gorm_plugin.go
@@ -0,0 +1,75 @@
+package psqlutil
+
+import (
+	"gorm.io/gorm"
+	"github.com/lib/pq"
+	"database/sql"
+	"github.com/purposeinplay/go-commons/errors"
+)
+
+type GORMErrorsPlugin struct{}
+
+func (p GORMErrorsPlugin) Name() string {
+	return "psqlerrors"
+}
+
+func (p GORMErrorsPlugin) Initialize(db *gorm.DB) (err error) {
+	cb := db.Callback()
+
+	return errors.Join(
+		cb.Create().After("gorm:create").Register("errors:after:create", p.handleError),
+		cb.Create().After("gorm:query").Register("errors:after:select", p.handleError),
+		cb.Create().After("gorm:delete").Register("errors:after:delete", p.handleError),
+		cb.Create().After("gorm:update").Register("errors:after:update", p.handleError),
+		cb.Create().After("gorm:row").Register("errors:after:row", p.handleError),
+		cb.Create().After("gorm:raw").Register("errors:after:raw", p.handleError),
+	)
+}
+
+func (p GORMErrorsPlugin) handleError(tx *gorm.DB) {
+	err := tx.Error
+
+	if err == nil {
+		return
+	}
+
+	const (
+		errCodeUniqueViolation = pq.ErrorCode("23505")
+		errCodeInvalidInput    = pq.ErrorCode("22P02")
+	)
+
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound), errors.Is(err, sql.ErrNoRows):
+		tx.Error = &errors.Error{
+			Type:    errors.ErrorTypeNotFound,
+			Details: "record not found",
+		}
+	case isErrorCode(err, errCodeInvalidInput):
+		tx.Error = &errors.Error{
+			Type:    errors.ErrorTypeInvalid,
+			Details: "invalid input",
+		}
+	case isErrorCode(err, errCodeUniqueViolation):
+		tx.Error = &errors.Error{
+			Type:    errors.ErrorTypeInvalid,
+			Details: "object already exists",
+		}
+	default:
+		tx.Error = &errors.Error{
+			Type:    errors.ErrorTypeInternalError,
+			Details: tx.Error.Error(),
+		}
+	}
+}
+
+// isErrorCode a specific postgres specific error as defined by
+// https://www.postgresql.org/docs/9.5/static/errcodes-appendix.html
+func isErrorCode(err error, errCode pq.ErrorCode) bool {
+	var pqErr *pq.Error
+
+	if errors.As(err, &pqErr) {
+		return pqErr.Code == errCode
+	}
+
+	return false
+}
