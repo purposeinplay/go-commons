70545523229973b5082b83236d25d5b617caaf1a
bradub <adrian.brad@purposeinplay.com>
2025-05-30 16:42:29 +0300

    fix(otel): enable grpc logging

--- otel/go.mod
+++ otel/go.mod
@@ -15,10 +15,12 @@ require (
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0
 	go.opentelemetry.io/contrib/instrumentation/runtime v0.61.0
 	go.opentelemetry.io/otel v1.36.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.12.2
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.36.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.36.0
-	go.opentelemetry.io/otel/exporters/stdout/stdoutlog v0.12.2
+	go.opentelemetry.io/otel/log v0.12.2
 	go.opentelemetry.io/otel/sdk v1.36.0
+	go.opentelemetry.io/otel/sdk/log v0.12.2
 	go.opentelemetry.io/otel/trace v1.36.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/sync v0.14.0
@@ -60,9 +62,7 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib v1.36.0 // indirect
-	go.opentelemetry.io/otel/log v0.12.2 // indirect
 	go.opentelemetry.io/otel/metric v1.36.0 // indirect
-	go.opentelemetry.io/otel/sdk/log v0.12.2 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.36.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- otel/signoz.go
+++ otel/signoz.go
@@ -5,22 +5,25 @@ import (
 	"errors"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
 	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/propagation"
 	otellog "go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 )
 
 func InitSigNozTracer(
 	serviceName string,
 	collectorURL string,
+	logCollectorURL string,
 	withTLS bool,
 ) func(context.Context) error {
 	secureOption := otlptracegrpc.WithInsecure()
@@ -67,21 +70,33 @@ func InitSigNozTracer(
 		),
 	)
 
-	exp, err := stdoutlog.New()
+	conn, err := grpc.NewClient(
+		logCollectorURL,
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to create stdout exporter: %v", err)
 	}
 
-	processor := otellog.NewSimpleProcessor(exp)
+	exp, err := otlploggrpc.New(
+		context.Background(),
+		otlploggrpc.WithGRPCConn(conn),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create stdout exporter: %v", err)
+	}
 
 	provider := otellog.NewLoggerProvider(
-		otellog.WithProcessor(processor),
+		otellog.WithProcessor(otellog.NewBatchProcessor(exp)),
 		otellog.WithResource(resources),
 	)
 
 	global.SetLoggerProvider(provider)
 
 	return func(ctx context.Context) error {
-		return errors.Join(provider.Shutdown(ctx), exporter.Shutdown(ctx))
+		return errors.Join(
+			provider.Shutdown(ctx),
+			exporter.Shutdown(ctx),
+			conn.Close(),
+		)
 	}
 }
